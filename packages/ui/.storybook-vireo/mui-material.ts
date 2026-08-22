1663cd180cd59a347c3f03151a22ea152479c8c0
Bruno Tot <brunotot10000@gmail.com>
2026-08-22 07:47:10 +0200

    fix(ui): expose dialog title to vireo storybook

--- packages/ui/.storybook-vireo/mui-material.ts
+++ packages/ui/.storybook-vireo/mui-material.ts
@@ -23,6 +23,7 @@ export { default as Container } from "@mui/material/Container";
 export { default as Dialog } from "@mui/material/Dialog";
 export { default as DialogActions } from "@mui/material/DialogActions";
 export { default as DialogContent } from "@mui/material/DialogContent";
+export { default as DialogTitle } from "@mui/material/DialogTitle";
 export { default as Divider } from "@mui/material/Divider";
 export { default as Drawer } from "@mui/material/Drawer";
 export { default as Fade } from "@mui/material/Fade";
