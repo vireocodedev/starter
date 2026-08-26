edf99ffc36a9c551bc0ef2cb00299d1a56a0957c
Bruno Tot <brunotot10000@gmail.com>
2026-08-26 02:33:58 +0200

    fix(ui): register action preview story icons

--- packages/ui/.storybook-vireo/mui-icons.ts
+++ packages/ui/.storybook-vireo/mui-icons.ts
@@ -2,6 +2,7 @@
 // Add an explicit re-export here only when a Vireo component imports that icon
 // from `@mui/icons-material`.
 export { default as Add } from "@mui/icons-material/Add";
+export { default as ArchiveOutlined } from "@mui/icons-material/ArchiveOutlined";
 export { default as ArrowBack } from "@mui/icons-material/ArrowBack";
 export { default as AspectRatioOutlined } from "@mui/icons-material/AspectRatioOutlined";
 export { default as CenterFocusStrong } from "@mui/icons-material/CenterFocusStrong";
@@ -10,6 +11,8 @@ export { default as CodeRounded } from "@mui/icons-material/CodeRounded";
 export { default as ContentCopy } from "@mui/icons-material/ContentCopy";
 export { default as DashboardOutlined } from "@mui/icons-material/DashboardOutlined";
 export { default as DarkModeOutlined } from "@mui/icons-material/DarkModeOutlined";
+export { default as DeleteOutlined } from "@mui/icons-material/DeleteOutlined";
+export { default as FilterAltOutlined } from "@mui/icons-material/FilterAltOutlined";
 export { default as Fullscreen } from "@mui/icons-material/Fullscreen";
 export { default as FullscreenExit } from "@mui/icons-material/FullscreenExit";
 export { default as HomeOutlined } from "@mui/icons-material/HomeOutlined";
@@ -17,10 +20,12 @@ export { default as Inventory2Outlined } from "@mui/icons-material/Inventory2Out
 export { default as LockOpenRounded } from "@mui/icons-material/LockOpenRounded";
 export { default as LockRounded } from "@mui/icons-material/LockRounded";
 export { default as MoreVert } from "@mui/icons-material/MoreVert";
+export { default as PublishOutlined } from "@mui/icons-material/PublishOutlined";
 export { default as Remove } from "@mui/icons-material/Remove";
 export { default as RestartAltRounded } from "@mui/icons-material/RestartAltRounded";
 export { default as SearchOffRounded } from "@mui/icons-material/SearchOffRounded";
 export { default as SearchRounded } from "@mui/icons-material/SearchRounded";
+export { default as SaveOutlined } from "@mui/icons-material/SaveOutlined";
 export { default as SettingsOutlined } from "@mui/icons-material/SettingsOutlined";
 export { default as TableRowsOutlined } from "@mui/icons-material/TableRowsOutlined";
 export { default as TuneRounded } from "@mui/icons-material/TuneRounded";
